10d18434960af8ccd90b26c6527073630f02acf0
Jessie <jessay0901@gmail.com>
2024-08-15 17:49:32 +1000

    rebuild with new domaim

--- server/server.js
+++ server/server.js
@@ -199,7 +199,7 @@ initializeDatabase().then(() => {
   console.log('Static files served from:', path.join(__dirname, '../build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
   app.listen(port, () => {
     console.log(`Server is running on ${process.env.REACT_APP_API_URL}:${port}`);
